c1173c87043f1c6e211e75d891bd3bc64828745b
Alena Chupina <chupina.elen@gmail.com>
2021-07-15 03:11:38 -0500

    add form validation

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer:{
+    contentBase: path.join(__dirname,"dist"),
+    port: 9000,
+    hot:true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+    template: './src/index.html'
+  }),
+  new CopyPlugin({
+    patterns: [
+      { from: "./src/assets/images", to: "./" }
+          ]})
+]
+};
