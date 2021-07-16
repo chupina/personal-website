11e17b32656b8c33ad12957c237ba543c51bd3ed
Alena Chupina <chupina.elen@gmail.com>
2021-07-16 15:42:08 -0500

    change validation pattern

--- src/email-validator.js
+++ src/email-validator.js
@@ -1,5 +1,5 @@
 const VALID_EMAIL_ENDINGS = ['gmail.com', 'outlook.com', 'yandex.ru']
 export const validate = (email) => {
-  let result = email.match(/@(.*)$/);
+  let result = email.trim().match(/\w+@(.*)$/);
   alert(result == null ? false : VALID_EMAIL_ENDINGS.includes(result[1]));
 };
