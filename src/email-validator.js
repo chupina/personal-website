05c73ab4ba1b57e0c8052da475e6f72dfcba8f82
Alena Chupina <chupina.elen@gmail.com>
2021-07-16 13:10:28 -0500

    improve email validator

--- src/email-validator.js
+++ src/email-validator.js
@@ -1,6 +1,5 @@
 const VALID_EMAIL_ENDINGS = ["gmail.com", "outlook.com", "yandex.ru"];
 export const validate = (email) => {
-  const result = email.match(/@(.*)$/);
-  // eslint-disable-next-line no-alert
-  alert(result == null ? false : VALID_EMAIL_ENDINGS.includes(result[1]));
+  const result = email.trim().match(/.+@(.*)$/);
+  return (result ? VALID_EMAIL_ENDINGS.includes(result[1]) : false);
 };
