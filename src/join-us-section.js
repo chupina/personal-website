8c3bf8c52e4995f78c9bcf40c855df7bb8935255
Alena Chupina <chupina.elen@gmail.com>
2021-07-19 21:26:52 -0500

    refactor Section class

--- src/join-us-section.js
+++ src/join-us-section.js
@@ -3,14 +3,14 @@ import { validate } from "./email-validator.js";
 const textPlaceholder =
   "Sed do eiusmod tempor incididunt <br> ut labore et dolore magna aliqua.";
 
-const setInputValue = (inputEl) => {
-  localStorage.setItem("email", inputEl.value);
+const setValue = (inputEl, key) => {
+  localStorage.setItem(key, inputEl.value);
 };
-const getInputValue = (inputEl, data) => {
-  if (!localStorage.getItem(data)) {
+const getValue = (inputEl, key) => {
+  if (!localStorage.getItem(key)) {
     inputEl.value = "";
   } else {
-    inputEl.value = localStorage.getItem(data);
+    inputEl.value = localStorage.getItem(key);
   }
 };
 
@@ -24,39 +24,65 @@ class Section {
     return this.element;
   }
 
-  renderContent(state) {
+  renderContent() {
     this.element.innerHTML = `<h2 class="app-title app-section__title--join">${this.headerContent}</h2>
      <h3 class="app-subtitle">${this.subheaderContent}</h3>
      <form class="app-section_form">
-         <input type="email" name="email" placeholder="Email" class="app-section__input--email ${state === "true" ? "hidden" : " "}">
-         <button type="submit" class="app-section__button app-section__input--subscribe">${state === "true" ? "unsubscribe" : this.buttonContent}</button>
+         <input type="email" name="email" placeholder="Email" class="app-section__input--email" autofocus>
+         <button type="submit" class="app-section__button app-section__input--subscribe">${this.buttonContent}</button>
      </form>`;
+    this.subscribeForm = this.element.querySelector("form");
+    this.emailInput = this.element.querySelector(".app-section__input--email");
+    this.submitButton = this.element.querySelector(".app-section__input--subscribe");
+  }
 
-    getInputValue(this.element.querySelector("input"), "email");
-    this.element.querySelector("form").addEventListener("submit", (e) => {
-      e.preventDefault();
-      e.stopPropagation();
-      if (localStorage.getItem("subscription") === "true") {
-        localStorage.clear();
-      } else if (localStorage.getItem("email")) {
-        const validationResult = validate(localStorage.getItem("email"));
-        localStorage.setItem("subscription", validationResult);
-        console.log(e.currentTarget.querySelector("input").value); // eslint-disable-line no-console
-      } else {
-        e.currentTarget.querySelector("input").classList.add("danger");
+  updateUI(state) {
+    if (state === "true") {
+      this.emailInput.classList.add("hidden");
+      this.emailInput.classList.remove("danger");
+      this.submitButton.textContent = "Unsubscribe";
+    } else {
+      this.emailInput.classList.remove("hidden");
+      this.submitButton.textContent = this.buttonContent;
+      if (state === "false") {
+        this.emailInput.classList.add("danger");
       }
-      this.renderContent(localStorage.getItem("subscription"));
-    });
-    this.element.querySelector("input").addEventListener("input", (e) => {
-      e.stopPropagation();
-      e.preventDefault();
-      setInputValue(e.target);
-    });
+    }
+    getValue(this.emailInput, "email");
+  }
+
+  checkState() {
+    const isSubscribed = localStorage.getItem("subscription");
+    this.updateUI(isSubscribed);
   }
 
   create() {
     this.element.className = "app-section app-section--image-join";
-    this.renderContent(localStorage.getItem("subscription"));
+    this.renderContent();
+    this.checkState();
+    getValue(this.emailInput, "email");
+    this.subscribeForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      if (localStorage.getItem("subscription") === "true") {
+        localStorage.clear();
+      }
+      if (localStorage.getItem("email")) {
+        const validationResult = validate(this.emailInput.value);
+        localStorage.setItem("subscription", validationResult);
+      }
+      this.checkState();
+      console.log(this.emailInput.value); // eslint-disable-line no-console
+    });
+    this.emailInput.addEventListener("input", (e) => {
+      e.stopPropagation();
+      e.preventDefault();
+      setValue(e.target, "email");
+    });
+    this.emailInput.addEventListener("focus", (e) => {
+      e.stopPropagation();
+      e.target.classList.remove("danger");
+    });
   }
 }
 
@@ -71,7 +97,7 @@ class AdvancedSection extends Section {
     super(
       "Join our advanced program",
       textPlaceholder,
-      "Subscribe to Advanced Program"
+      "Subscribe to Advanced Program",
     );
   }
 }
