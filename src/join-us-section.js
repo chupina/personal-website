2d0913a7c092f602120408f3f6341c16d98b350e
Alena Chupina <chupina.elen@gmail.com>
2021-07-29 18:42:40 +0000

    Storage task

--- src/join-us-section.js
+++ src/join-us-section.js
@@ -1,6 +1,18 @@
 import { validate } from "./email-validator.js";
 
-const textPlaceholder = "Sed do eiusmod tempor incididunt <br> ut labore et dolore magna aliqua.";
+const textPlaceholder =
+  "Sed do eiusmod tempor incididunt <br> ut labore et dolore magna aliqua.";
+
+const setValue = (inputEl, key) => {
+  localStorage.setItem(key, inputEl.value);
+};
+const getValue = (inputEl, key) => {
+  if (!localStorage.getItem(key)) {
+    inputEl.value = "";
+  } else {
+    inputEl.value = localStorage.getItem(key);
+  }
+};
 
 class Section {
   constructor(headerContent, subheaderContent, buttonContent) {
@@ -12,20 +24,64 @@ class Section {
     return this.element;
   }
 
+  renderContent() {
+    this.element.innerHTML = `<h2 class="app-title app-section__title--join">${this.headerContent}</h2>
+     <h3 class="app-subtitle">${this.subheaderContent}</h3>
+     <form class="app-section_form">
+         <input type="email" name="email" placeholder="Email" class="app-section__input--email" autofocus>
+         <button type="submit" class="app-section__button app-section__input--subscribe">${this.buttonContent}</button>
+     </form>`;
+    this.subscribeForm = this.element.querySelector("form");
+    this.emailInput = this.element.querySelector(".app-section__input--email");
+    this.submitButton = this.element.querySelector(".app-section__input--subscribe");
+  }
+
+  updateUI(state) {
+    if (state === "true") {
+      this.emailInput.classList.add("hidden");
+      this.emailInput.classList.remove("danger");
+      this.submitButton.textContent = "Unsubscribe";
+    } else {
+      this.emailInput.classList.remove("hidden");
+      this.submitButton.textContent = this.buttonContent;
+      if (state === "false") {
+        this.emailInput.classList.add("danger");
+      }
+    }
+    getValue(this.emailInput, "email");
+  }
+
+  checkState() {
+    const isSubscribed = localStorage.getItem("subscription");
+    this.updateUI(isSubscribed);
+  }
+
   create() {
     this.element.className = "app-section app-section--image-join";
-    this.element.innerHTML = `<h2 class="app-title app-section__title--join">${this.headerContent}</h2>
-                            <h3 class="app-subtitle">${this.subheaderContent}</h3>
-                            <form class="app-section_form">
-                                <input type="email" placeholder="Email" class="app-section__input--email">
-                                <button type="submit" class="app-section__button app-section__input--subscribe">${this.buttonContent}</button>
-                            </form>`;
-
-    this.element.querySelector("form").addEventListener("submit", (e) => {
+    this.renderContent();
+    this.checkState();
+    getValue(this.emailInput, "email");
+    this.subscribeForm.addEventListener("submit", (e) => {
       e.preventDefault();
       e.stopPropagation();
-      console.log(e.currentTarget.querySelector("input").value);// eslint-disable-line no-console
-      validate(e.currentTarget.querySelector("input").value);
+      if (localStorage.getItem("subscription") === "true") {
+        localStorage.clear();
+      }
+      if (localStorage.getItem("email")) {
+        const validationResult = validate(this.emailInput.value);
+        localStorage.setItem("subscription", validationResult);
+      }
+      this.checkState();
+      console.log(this.emailInput.value); // eslint-disable-line no-console
+    });
+    this.emailInput.addEventListener("input", (e) => {
+      e.stopPropagation();
+      e.preventDefault();
+      setValue(e.target, "email");
+    });
+    this.emailInput.addEventListener("focus", (e) => {
+      e.stopPropagation();
+      e.target.classList.remove("danger");
     });
   }
 }
