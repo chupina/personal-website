6888d8e3bf52a692b393da36484b59788193aa28
Alena Chupina <chupina.elen@gmail.com>
2021-08-24 23:59:37 +0000

    Web components task

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import "./styles/style.css";
 import { SectionCreator } from "./join-us-section.js";
 import { CommunitySection } from "./community-section.js";
 import { Renderer } from "./renderer.js";
+import { WebsiteSection } from "./website-section.js";
+
+customElements.define("website-section", WebsiteSection);
 
 const sectionFactory = new SectionCreator();
 const standardSection = sectionFactory.create("standard");
 const communitySection = new CommunitySection();
 const sectionRendererJoin = new Renderer(standardSection, ".placeholder-join");
-const sectionRendererCommunity = new Renderer(communitySection, ".placeholder-community");
+const sectionRendererCommunity = new Renderer(
+  communitySection,
+  ".placeholder-community",
+);
 
 window.addEventListener("load", sectionRendererJoin.render);
 window.addEventListener("load", sectionRendererCommunity.render);
